ea66eb2f532ef60dbcd4d7248e177896a0c51dfa
micheal965 <micheal.magdy69@yahoo.com>
2025-09-09 07:27:40 +0300

    init

---
+++ NeetCode/Day 2/GroupAnagrams.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem_Solving.NeetCode.Day_2
+{
+    class GroupAnagrams
+    {
+
+        //    var result = GroupAnagrams(["act", "pots", "tops", "cat", "stop", "hat"]);
+        //        foreach (var item in result)
+        //        {
+        //            foreach (var item1 in item)
+        //            {
+        //                Console.WriteLine(item1);
+        //            }
+        //Console.WriteLine("------------");
+        //        }
+
+
+
+        //public static List<List<string>> GroupAnagrams(string[] strs)
+        //{
+        //    var dict = new Dictionary<string, List<string>>();
+
+        //    foreach (var str in strs)
+        //    {
+        //        var sorted = new string(str.OrderBy(c => c).ToArray());
+
+        //        if (!dict.ContainsKey(sorted))
+        //            dict[sorted] = new List<string>();
+
+        //        dict[sorted].Add(str);
+        //    }
+
+        //    return dict.Values.ToList();
+        //}
+    }
+}
---
+++ NeetCode/Day 2/TopKFrequent.cs
@@ -0,0 +1,21 @@
+﻿namespace Problem_Solving.NeetCode.Day_2
+{
+    class TopKFrequent
+    {
+        //TopKFrequent([1, 2, 2, 3, 3, 3], 2);
+
+        //public static int[] TopKFrequent(int[] nums, int k)
+        //{
+        //    var dict = new Dictionary<int, int>();
+
+        //    foreach (var item in nums)
+        //    {
+        //        if (!dict.ContainsKey(item))
+        //            dict[item] = 0;
+
+        //        dict[item]++;
+        //    }
+        //    return dict.OrderByDescending(d => d.Key).Select(s => s.Key).Take(k).ToArray();
+        //}
+    }
+}
