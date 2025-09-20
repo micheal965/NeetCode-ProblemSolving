a5ef621436da085884c05d2f09ee79d98bfbd019
micheal965 <micheal.magdy69@yahoo.com>
2025-09-20 04:07:16 +0300

    feat: Solved Day 9 Problems

---
+++ NeetCode/Day 9/LengthOfLongestSubstring.cs
@@ -0,0 +1,53 @@
+﻿namespace Problem_Solving.NeetCode.Day_9
+{
+    internal class LengthOfLongestSubstring
+    {
+
+        //Program program = new Program();
+        //program.LengthOfLongestSubstring("abcabcbb");
+        //program.LengthOfLongestSubstring(" ");
+
+        //Input: s = "zxyzaxyz"
+        //Output: 3
+
+        //Input: s = "xxxx"
+        //Output: 1
+        //public int LengthOfLongestSubstring(string s)
+        //{
+        //    int length = 0;
+        //    var hashset = new HashSet<char>();
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        for (int j = i; j < s.Length; j++)
+        //        {
+        //            if (!hashset.Contains(s[j]))
+        //                hashset.Add(s[j]);
+        //            else
+        //            {
+        //                length = Math.Max(length, hashset.Count);
+        //                hashset.Clear();
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    return length;
+        //}
+        //public int LengthOfLongestSubstring(string s)
+        //{
+        //    int MaxLength = 0, left = 0;
+        //    var hashset = new HashSet<char>();
+
+        //    for (int right = 0; right < s.Length; right++)
+        //    {
+        //        while (hashset.Contains(s[right]))
+        //        {
+        //            hashset.Remove(s[left]);
+        //            left++;
+        //        }
+        //        hashset.Add(s[right]);
+        //        MaxLength = Math.Max(MaxLength, right - left + 1);
+        //    }
+        //    return MaxLength;
+        //}
+    }
+}
---
+++ NeetCode/Day 9/MaxProfit.cs
@@ -0,0 +1,26 @@
+﻿namespace Problem_Solving.NeetCode.Day_9
+{
+    internal class MaxProfit
+    {
+
+        //Program program = new Program();
+        //program.MaxProfit([10, 1, 5, 6, 7, 1]);
+        //program.MaxProfit([7, 6, 4, 3, 1]);
+
+        //public int MaxProfit(int[] prices)
+        //{
+        //    int sell, buy, profit = 0;
+        //    for (int i = 0; i < prices.Length; i++)
+        //    {
+        //        buy = prices[i];
+        //        for (int j = i + 1; j < prices.Length; j++)
+        //        {
+        //            sell = prices[j];
+        //            if (profit < sell - buy)
+        //                profit = sell - buy;
+        //        }
+        //    }
+        //    return profit;
+        //}
+    }
+}
