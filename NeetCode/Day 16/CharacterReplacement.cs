d51ad13fbd31221d2c94778f7cde3392c949577e
micheal965 <micheal.magdy69@yahoo.com>
2025-10-05 09:15:14 +0300

    feat: Solved day 16 problems

---
+++ NeetCode/Day 16/CharacterReplacement.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem_Solving.NeetCode.Day_16
+{
+    internal class CharacterReplacement
+    {
+        //public int CharacterReplacement(string s, int k)
+        //{
+        //    int res = 0;
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        Dictionary<char, int> count = new Dictionary<char, int>();
+        //        int maxf = 0;
+        //        for (int j = i; j < s.Length; j++)
+        //        {
+        //            if (count.ContainsKey(s[j]))
+        //                count[s[j]]++;
+        //            else
+        //                count[s[j]] = 1;
+
+        //            maxf = Math.Max(maxf, count[s[j]]);
+        //            if ((j - i + 1) - maxf <= k)
+        //                res = Math.Max(res, j - i + 1);
+        //        }
+        //    }
+        //    return res;
+        //}
+    }
+}
--- obj/Debug/net8.0/Problem Solving.AssemblyInfo.cs
+++ obj/Debug/net8.0/Problem Solving.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem Solving")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd536b710dfcf7749813cdaaa983660a4f0f5ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e08cd8c0abe4a36ede1076ca7916fc3ebc5ff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem Solving")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem Solving")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
